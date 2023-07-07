1177bc641fe0e2b261aeec9223addabe7c846904
colmpat <cplang@dons.usfca.edu>
2023-07-07 13:26:09 -0700

    feat: serve requests to /[word]

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -1,6 +1,10 @@
 package server
 
-import "time"
+import (
+	"encoding/json"
+	"net/http"
+	"strings"
+)
 
 type Server struct {
 }
@@ -10,6 +14,28 @@ func NewServer() *Server {
 }
 
 func (s *Server) Start() {
-	// listen on port 8080
-	time.Sleep(10 * time.Second)
+	http.HandleFunc("/", handleRequest)
+	http.ListenAndServe(":8080", nil)
+}
+
+func handleRequest(w http.ResponseWriter, r *http.Request) {
+	word := extractSearchTerm(r.URL.Path)
+	if word == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	json.NewEncoder(w).Encode(map[string]string{
+		"word": word,
+	})
+}
+
+// extractSearchTerm extracts the search term from the URL path. eg. /hello -> hello
+func extractSearchTerm(path string) string {
+	parts := strings.Split(path, "/")
+	if len(parts) < 2 {
+		return ""
+	}
+
+	return parts[1]
 }
